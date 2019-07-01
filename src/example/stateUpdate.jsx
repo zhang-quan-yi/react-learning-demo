d2c3405bd0f9922fbc6475c51f2f745d2af322da
zhang-quan-yi <zh.hailei@gmail.com>
2019-07-01 08:27:51 +0800

    add state update demo

---
+++ src/example/stateUpdate.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default class StateUpdate extends React.Component {
+    state = {
+        person: {
+            info: {
+                name: 'hello'
+            }
+        }
+    };
+    updateName1 = () => {
+        this.state.person.info.name = 'world' + Math.random();
+        this.setState({});
+    };
+
+    updateName2 = () => {
+        let {person} = this.state;
+
+        person.info.name = 'world' + Math.random();
+        this.setState({
+            person: {
+                ...person
+            }
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h3>Component State Update</h3>
+                <button onClick={this.updateName1}>no replace person object</button>
+                <button onClick={this.updateName2}>replace person object</button>
+                <div>{this.state.person.info.name}</div>
+            </div>
+        );
+    }
+}
